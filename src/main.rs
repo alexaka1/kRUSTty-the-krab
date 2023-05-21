ff3e04b5d523d0f876bbf0b4becaef6a143cd71c
Martossy Alex <github@mail.martossy.hu>
2023-05-21 17:58:53 +0200

    reading stdio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// use std::io;
+
 fn main() {
     println!("Sugondese");
+
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    // so what am I doing? Is this basically a malloc for a string? but it's growable
+    let mut guess: String = String::new();
+    std::io::stdin()
+        // the variable literally switches a stack, wow
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    // this makes me question why this isn't the default string syntax in every language. For C# it's opt-in with $""
+    println!("You guess: {guess}");
 }
